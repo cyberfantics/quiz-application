56ce80b7219df941bc0dd0ec74f26d05354927fe
Cyber Fantics <147060613+cyberfantics@users.noreply.github.com>
2024-08-17 15:55:39 +0500

    Complete QuizApp Class Format

--- main.py
+++ main.py
@@ -16,3 +16,27 @@ class QuizApp:
 
     def menu_error(self):
         print("That's not a valid Sellection, Please Try Again")
+        
+    def goodbye(self):
+        print(f'''
+        -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
+        ~~~ Thank You For Using PyQuiz, {self.username}! ~~~
+        -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
+''')
+        print()
+
+    def menu(self):
+        self.menu_header()
+
+        # Run untill the user exit the app
+        selection = ''
+
+        # Entry Point of the program
+    def run(self):
+        self.startup()
+        self.menu()
+
+
+if __name__ == "__main__":
+    app = QuizApp()
+    app.run()
