6795f5c82de29360fb49d1424edcdec0c733fbf5
Cyber Fantics <147060613+cyberfantics@users.noreply.github.com>
2024-08-17 17:06:33 +0500

    Added Quiz Questions and Answers Class

--- main.py
+++ main.py
@@ -86,7 +86,80 @@ class QuizApp:
         self.startup()
         self.menu()
 
+# Implement Question Class
+class Question:
+    def __init__(self):
+    
+    # TODO Define the Question Fields
+        self.points = 0
+        self.correct_answer = ''
+        self.text = ''
+        self.is_correct = False
 
+
+# Inherit Question Class Into True False Class
+class QuestionTF(Question):
+    def __init__(self):
+        super().__init__()
+    
+    # TODO Define The Asking Method
+
+    def ask(self):
+        while True:
+            print(f'(T)rue or (F)alse: {self.text}')
+            response = input("? ")
+
+            # TODO: Check to see if no response was entered
+            if len(response) == 0:
+                print("Not a valid Response, Continue")
+                continue
+            
+            # TODO: Check to see if either T or F was given
+            if response[0].lower() != 't' and response[0].lower()!='f':
+                print("Not a valid Response, Continue")
+                continue
+
+            # TODO: Mark this question as correct, if answered correctly
+            if response[0].lower() == self.correct_answer:
+                self.is_correct = True
+
+            break
+
+
+# Inherit Question Class Into MCQs Class
+class QuestionMCQs(Question):
+    def __init__(self):
+        super().__init__()
+        # TODO: Define the answer for asking question
+        self.answers = []
+
+    def ask(self):
+        while True:
+            print(f"{self.text} ")
+            for a in self.answer:
+                print(f"({a.name}). {a.text}")
+            response = input("? ")
+
+            # TODO: Check to see if no response was entered
+            if len(response) == 0:
+                print("Not a valid Response, Continue")
+                continue
+            
+
+            # TODO: Mark this question as correct, if answered correctly
+            if response[0].lower() == self.correct_answer:
+                self.is_correct = True
+
+            break
+
+
+# Answer Class For Multiple Choice Question
+class Answer():
+    def __init__(self):
+        self.text = ''
+        self.name = ''
+
+        
 if __name__ == "__main__":
     app = QuizApp()
     app.run()
